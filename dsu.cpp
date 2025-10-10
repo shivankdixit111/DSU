3c061c569ae99bd697dbcfb3e98991a63bf8cf7f
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2025-10-10 08:06:48 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -55,10 +55,10 @@ template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a)
 
   
  
- class DisjointSet { //tells whether u and v belong to same component in constant time O(1)--//
+ class DSU { //tells whether u and v belong to same component in constant time O(1)--//
   public:
    vector<int> rank,parent,size;
-    DisjointSet(int n) {
+    DSU(int n) {
        rank.resize(n+1,0);
        parent.resize(n+1);
        size.resize(n+1,1);
